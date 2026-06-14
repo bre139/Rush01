482dfae1635803df65b0bacfb0651fd95ece33c4
Alper Kadiroglu <akadirog@f0r1s15.codam.nl>
2026-06-14 15:50:30 +0200

    put.c modified. now prints out matrix correctly

--- ex00/put.c
+++ ex00/put.c
@@ -6,32 +6,46 @@
 /*   By: akorthou <akorthou@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/06/14 02:05:10 by akorthou      #+#    #+#                 */
-/*   Updated: 2026/06/14 04:24:01 by akorthou      ########   odam.nl         */
+/*   Updated: 2026/06/14 15:49:14 by akadirog      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+// #include <stdio.h> // TO BE REMOVED
 
 void put_matrix(int matrix[4][4], int n)
 {
-	int	i;
-	int j;
+	int	row;
+	int col;
 	char c;
 
-	j = 0;
-	i = n-1;
+	row = 0;
 	c = '0';
-	while (j < n)
+	while (row < n)
 	{
-		while (i >= 0)
+		col = 0;
+		while (col < n)
 		{
-				c = matrix[i][j]+'0';
+				c = matrix[row][col]+'0';
 				write(1,&c,1);
-				i--;
+				col++;
 		}
 		c = '\n';
 		write(1,&c,1);
-		j++;
-		i=n-1;
+		row++;
 	}
 }
+
+// TO BE REMOVED
+/* int main(void)
+{
+	int grid[4][4] = {
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+		{6, 3, 0, 0},
+		{0, 0, 3, 4}
+	};
+	
+	put_matrix(grid, 4);
+	return (0);
+} */
